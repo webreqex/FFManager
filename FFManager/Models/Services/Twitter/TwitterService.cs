052af26daccceb17b7c03c9baff0491e972f58dc
a32kita <a32kita@yahoo.co.jp>
2017-11-15 22:48:19 +0900

    TwitterServiceの定義を開始

---
+++ FFManager/Models/Services/Twitter/TwitterService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FFManager.Models.Bases;
+
+namespace FFManager.Models.Services.Twitter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TwitterService : ServiceBase
+    {
+
+    }
+}
