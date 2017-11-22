6503545e03d87080773328836fa7ecad6bbe71cc
a32kita <a32kita@yahoo.co.jp>
2017-11-23 00:58:50 +0900

    AuthorizePanel の画面遷移を実装してみる

---
+++ FFManager/Views/ViewModels/AuthorizePanelVMElements/AuthorizePanelState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFManager.Views.ViewModels.AuthorizePanelVMElements
+{
+    /// <summary>
+    /// AuthorizePanel の状態の示す値を定義します。
+    /// </summary>
+    public enum AuthorizePanelState : Int32
+    {
+        /// <summary>
+        /// サービスを選択している画面
+        /// </summary>
+        ServiceSelect = 1,
+
+        /// <summary>
+        /// 選択したサービスにログインする画面
+        /// </summary>
+        ServiceLogin = 2,
+    }
+}
