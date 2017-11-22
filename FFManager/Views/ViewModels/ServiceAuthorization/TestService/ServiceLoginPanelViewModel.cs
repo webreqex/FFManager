608de7e20fd26f061389aa2895b1deffede61bef
webreqex <twitter_webex@a32-test02.ml>
2017-11-23 01:22:35 +0900

    各サービスへのログイン用UIについて、どの形式でAuthorizePanelViewModelで渡すか検討中

---
+++ FFManager/Views/ViewModels/ServiceAuthorization/TestService/ServiceLoginPanelViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFManager.Views.ViewModels.ServiceAuthorization.TestService
+{
+    public class ServiceLoginPanelViewModel : ServiceAuthorizationPanelViewModelBase
+    {
+
+    }
+}
