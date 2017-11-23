76135a9215c71c8301d2691e379c6097d13e6970
Redistributable <a32real@yahoo.co.jp>
2017-11-24 07:14:30 +0900

    認証用パネルの基礎を実装

---
+++ FFManager/Models/AuthorizeViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+using LocusCommon.Windows.ViewModels;
+
+namespace FFManager.Models
+{
+    /// <summary>
+    /// 各サービスのログイン認証用のビューのビューモデルの共通基底クラスを定義します。
+    /// </summary>
+    public class AuthorizeViewModelBase : ExtendedViewModelBase
+    {
+        // 非公開フィールド
+
+
+        // 公開プロパティ
+
+        public Control Target
+        {
+            get => this.GetBindingValue<Control>(nameof(this.Target));
+            set => this.SetBindingValue(nameof(this.Target), value)
+        }
+    }
+}
---
+++ FFManager/Models/Services/Tests/AuthorizeViews/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LocusCommon.Windows.ViewModels;
+
+namespace FFManager.Models.Services.Tests.AuthorizeViews
+{
+    /// <summary>
+    /// TestService の AuthorizeView 用のビューモデルを定義します。
+    /// </summary>
+    public class ViewModel : AuthorizeViewModelBase
+    {
+
+    }
+}
