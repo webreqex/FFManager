b389da6e75e077c510852d09d82ec4b944633c99
a32kita <a32kita@yahoo.co.jp>
2017-11-26 23:52:42 +0900

    Childrenがバインド出来るタイプの擬似Gridコントロールの実装について検討

---
+++ FFManager/Views/Controls/Commons/ChildrenBindableGrid.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FFManager.Views.Controls.Commons
+{
+    /// <summary>
+    /// ChildrenBindableGrid.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ChildrenBindableGrid : UserControl
+    {
+        // 依存関係プロパティ
+        
+
+
+        // コンストラクタ
+
+        /// <summary>
+        /// 新しい ChildrenBindableGrid クラスのインスタンスを初期化します。
+        /// </summary>
+        public ChildrenBindableGrid()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        // 公開メソッド
+
+    }
+}
